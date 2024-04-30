ba9e871b31812220c5f04a0ca4f989304232321b
Aditya Pradhan <136620407+adityapradhan202@users.noreply.github.com>
2024-05-01 03:42:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from mess_data import mayuri_mess_data as mmd
+import time
+from day_time import wday_dict
+from day_time import current_weekday
+from day_time import current_hour
+
+current_meal = ""
+if current_hour <= 9:
+    current_meal = "Breakfast"
+elif current_hour <= 14:
+    current_meal = "Lunch"
+elif current_hour <= 18:
+    current_meal = "Snacks"
+elif current_hour <= 21:
+    current_meal = "Dinner"
+elif current_hour > 21:
+    current_meal = "Breakfast"
+
+
+st.header("Welcome to VBMESS :pizza:")
+
+st.markdown("##### Select the day and the meal type and get the respective food items of the menu. Whether you're in the mood for a hearty lunch or a light snack, we've got you covered.")
+st.write("##### So go ahead, explore the menus, and satisfy your taste buds!")
+st.write("##### Happy eating! :smile:")
+
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+meals = ["Breakfast", "Lunch", "Snacks", "Dinner"]
+
+c1,c2 = st.columns(2)
+with c1:
+    selected_day = st.selectbox(label="Select day", options=days, index=days.index(wday_dict[current_weekday]))
+with c2:
+    selected_meal = st.selectbox(label="Select meal", options=meals, index=meals.index(current_meal))
+
+
+if selected_meal != None and selected_day != None:
+    st.markdown(f"##### Here are your food items for {selected_meal} of {selected_day}")
+    for x, i in enumerate(mmd[selected_day][selected_meal]):
+        st.write(f"{x+1}) {i}")
+        time.sleep(0.25)
+    
+    
+with st.sidebar:
+    st.write("Some information about the developer...")
+    b = st.link_button(label="Developer's Github", url='https://github.com/adityapradhan202', type='secondary')
+    a = st.link_button(label="Developer's LinkedIn", url='https://www.linkedin.com/in/adipradhan202', type='secondary')
+
+
+            
---
+++ mess_data.py
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+
+# Data used in the app ------------------------
+mayuri_mess_data = {
+    "Monday":
+    {
+        "Breakfast":["Idly", "Sambar, Chutney", "Banana/Cut-fruit", "Bread-Butter-Jam", "Milk-Tea-Coffee"],
+        "Lunch":["Fry-yums", "Roti", "Channa Dal", "Vegetable Khichdi/Lemon Rice", "Aloo Cabage Masala", " Rice", "Rasam", "Butter Milk"],
+        "Snacks":["Kachori/Noodles", "Sauce/Chutney", "Milk-Tea-Coffee"],
+        "Dinner":["Roti", "Bhindi do pyaza", "Egg Bhurji Gravy", "Rice", "Sambar", "Pickle"]
+    },
+    "Tuesday":{
+        "Breakfast":["Poori", "Bhaji", "Banana/Cut-Fruit", "Bread-Butter-Jam", "Milk-Tea-Coffee"],
+        "Lunch":["Allo Mutter Masala", "Roti", "Salad", "Rice", "Dal Tadka", "Sambar", "Ginjer Rassam"],
+        "Snacks":["Pani Poori", "Sauce/Chutney", "Milk-Tea-Coffee"],
+        "Dinner":["Roti", "Kuska", "Sev Tamatar", "Masala Dal", "Rice", "Rasam", "Pickle", "Fruit Custard/Halwa"]
+    },
+    "Wednesday":{
+        "Breakfast":["BOILED EGGS", "Onion Veg Uthappam", "Sambar-Tomato Chutney", "Banana/Cut-Fruit", "Bread-Butter-Jam", "Milk-Tea-Coffee"],
+        "Lunch":["Cabbage Channa Dal", "Mix Dal", "Payasam(Kheer)/Boondi", "Raita"],
+        "Snacks":["Samosa/Chana Sundal", "Sauce/Chutney", "Milk-Tea-Coffee"],
+        "Dinner":["Roti", "Rice", "Plain Dal", "CHICKEN MASALA", "PANEER MASALA", "Rasam", "Pickle"]
+    },
+    "Thursday":{
+        "Breakfast":["Poha", "Jalebi", "Pongal-Sambar", "Kara Chutney", "Banana/Cut-Fruit", "Bread-Butter-Jam", "Milk-Tea-Coffee"],
+        "Lunch":["Roti", "Veg Kofta", "Tomato Dal", "Veg Pulao", "Plain Rice", "Sambar", "Beetroot Poriyal", "Tomato Rasam", "Curd/Juice"],
+        "Snacks":["Sweet corn salad/Burger","Sauce/Chutney", "Milk-Tea-Coffee"],
+        "Dinner":["Roti", "Seasonal Veg", "Egg Masala", "Rice", "Dal Fry", "Rasam", "Pickle"]
+    },
+    "Friday":{
+        "Breakfast":["Bhatura", "Chole Masala", "Banana/Cut-Fruit", "Bread-Butter-Jam", "Milk-Tea-Coffee"],
+        "Lunch":["Veg Biriyani", "Roti", "Brinjal Masala/Dum aloo", "Plain Rice", "Sambhar Rice", "Raw Banana Poriyal", "Rasam", "Masoor Dal", "Boondi Raita"],
+        "Snacks":["Dahi Vada/Bhaji", "Sauce/Chutney", "Milk-Tea-Coffee"],
+        "Dinner":["Roti", "Rice", "Dal Tadka", "Rasam", "Pickle", "BUTTER CHICKEN", "KADAI PANEER"]
+    },
+    "Saturday":{
+        "Breakfast":["Rava Upma & Pav Bhaji", "Chutney", "Banana/Cut-Fruit", "Bread-Butter-Jam", "Milk-Tea-Coffee"],
+        "Lunch":["Salad", "Roti", "Rajma Gharwala", "Jeera Rice/Rice", "Seasonal Veg", "Pepper Rasam", "Potato Poriyal" "Butter Milk"],
+        "Snacks":["Cutlet/Daveli", "Sauce/Chutney", "Tea-Coffee-Milk"],
+        "Dinner":["Roti", "Jeera Rice", "Dal Fry", "Veg 65", "Sev Tamatar/Seasonal Veg", "Rice", "Sambhar", "Pickle"]
+    },
+    "Sunday":{
+        "Breakfast":["Masala Dosa", "Sambhar-Chutney", "Banana-Cut-Fruit", "Bread", "Butter-Jam", "Milk-Tea-Coffee"],
+        "Lunch":["Roti", "CHICKEN BIRIYANI", "Veg Biriyani", "Masala Dal/MUTTER PANEER", "Juice", "Onion Cucumber Raita"],
+        "Snacks":["Dhokla/Pasta", "Sauce/Chutney", "Tea-Coffee-Milk"],
+        "Dinner":["Roti", "Rice", "Pickle", "Veg Kholapuri", "Masoor Dal/Soua Masala", "Rasam", "Gulab Jamun/Pineapple Halwa"]
+    }
+
+}
+
+
+
